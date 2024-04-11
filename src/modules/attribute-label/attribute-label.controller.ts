7bd182565820f2bac397aa371810bcf1d077fb84
fsgdoterr <fsgdoterr@gmail.com>
2024-04-11 17:01:31 +0300

    Added update attribute label endpoint

--- src/modules/attribute-label/attribute-label.controller.ts
+++ src/modules/attribute-label/attribute-label.controller.ts
@@ -1,9 +1,10 @@
-import { Body, Controller, HttpCode, HttpStatus, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Param, Post, Put, UseGuards } from '@nestjs/common';
 import { AttributeLabelService } from './attribute-label.service';
 import AccessJwtGuard from 'src/common/guards/access-jwt.guard';
 import ModerRoleGuard from 'src/common/guards/moder-role.guard';
 import { User } from 'src/common/decorators/user.decorator';
 import CreateAttributeLabelDto from './dtos/create-attribute-label.dto';
+import UpdateAttributeLabelDto from './dtos/update-attribute-label.dto';
 
 @Controller('attribute-label')
 export class AttributeLabelController {
@@ -19,4 +20,12 @@ export class AttributeLabelController {
         return await this.attrLabelService.create(dto);
     }
 
+    @Put('/:id')
+    @UseGuards(AccessJwtGuard,ModerRoleGuard)
+    async update(
+        @Param('id') labelId: string,
+        @Body() dto: UpdateAttributeLabelDto
+    ) {
+        return await this.attrLabelService.update(labelId, dto);
+    }
 }
--- src/modules/attribute-label/attribute-label.service.ts
+++ src/modules/attribute-label/attribute-label.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { AttributeRepositoryService } from 'src/services/repositories/attribute-repository/attribute-repository.service';
 import CreateAttributeLabelDto from './dtos/create-attribute-label.dto';
+import UpdateAttributeLabelDto from './dtos/update-attribute-label.dto';
 
 @Injectable()
 export class AttributeLabelService {
@@ -15,6 +16,23 @@ export class AttributeLabelService {
         const newAttributeLabel = await this.attrRepository.createLabel(categoryId, title);
 
         return newAttributeLabel.getResponseDto();
+    }
+
+    async update(
+        labelId: string,
+        {title}: UpdateAttributeLabelDto
+    ) {
+        const label = await this.attrRepository.getLabelById(labelId);
+        console.log(label);
+        
+        if(!label)
+            throw new BadRequestException('Invalid id');
+
+        label.title = title;
+
+        await label.save();
+
+        return label.getResponseDto();
     }
 
 }
