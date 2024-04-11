a9dfddd53005c453bf4db3b2d4001bc34e9365fa
fsgdoterr <fsgdoterr@gmail.com>
2024-04-11 12:27:55 +0300

    Added methods to user repository

--- src/services/repositories/user-repository/user-repository.service.ts
+++ src/services/repositories/user-repository/user-repository.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { ERole } from 'src/common/constants/role';
 import InjectModel from 'src/common/decorators/inject-model.decorator';
 import CartItemEntity from 'src/common/entities/cart-item/cart-item.entity';
 import UserEntity from 'src/common/entities/user/user.enity';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserRepositoryService {
@@ -11,5 +13,32 @@ export class UserRepositoryService {
         @InjectModel(CartItemEntity)
         private readonly cartItemModel: typeof CartItemEntity,
     ) {}
+
+    async isEmailExists(email: string) {
+        return !!(await this.userModel.findOne({where: { email }}));
+    }
+
+    async register(
+        email: string,
+        password: string,
+        role: ERole = ERole.USER
+    ) {
+        const hashPassword = await bcrypt.hash(password, 10);
+        const newUser = await this.userModel.create({
+            email,
+            password: hashPassword,
+            role
+        });
+
+        return newUser;
+    }
+
+    getUserModel() {
+        return this.userModel;
+    }
+
+    getCartItemModel() {
+        return this.cartItemModel;
+    }
 
 }
